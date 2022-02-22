ae085d83cc6f6e196a3acc4402059d4b4fc138e2
DanaZhark <26148@edu.iitu.kz>
2022-02-22 21:20:47 +0600

    Initial commit

---
+++ src/main/java/com/zhandabo/overgame/OvergameApplication.java
@@ -0,0 +1,13 @@
+package com.zhandabo.overgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OvergameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OvergameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhandabo/overgame/OvergameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhandabo.overgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OvergameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
